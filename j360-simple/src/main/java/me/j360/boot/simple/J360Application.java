7de1146c2aeb6b4f8d7f2d7935f1f96adff26ab6
xumin <xumin_wlt@163.com>
2015-09-23 21:54:54 +0800

    add simple spring web and jsp view

---
+++ j360-simple/src/main/java/me/j360/boot/simple/J360Application.java
@@ -0,0 +1,19 @@
+package me.j360.boot.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created with springbootweb -> me.j360.springboot.jar.
+ * User: min_xu
+ * Date: 2015/7/28
+ * Time: 15:28
+ * 说明：
+ */
+public class J360Application {
+    public static void main(String[] args) {
+        SpringApplication.run(J360Configuration.class, args);
+    }
+
+
+}
---
+++ j360-simple/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+	Hello J360-boot
+</body>
+</html>
